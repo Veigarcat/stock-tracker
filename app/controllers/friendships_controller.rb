1ea66914707619117873242a2d34561f3e07a1c3
manhminno <manh6264@gmail.com>
2020-11-30 22:27:14 +0700

    Create friendship tracking from browser

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,19 @@
 class FriendshipsController < ApplicationController
-    def create
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following friend"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
     end
-  
-    def destroy
-      friendship = current_user.friendships.where(friend_id: params[:id]).first
-      friendship.destroy
-      flash[:notice] = "Stopped following"
-      redirect_to my_friends_path
-    end
+    redirect_to my_friends_path
   end
+
+  def destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+    flash[:notice] = "Stopped following"
+    redirect_to my_friends_path
+  end
+end
