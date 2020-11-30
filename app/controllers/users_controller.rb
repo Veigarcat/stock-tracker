7b512c44b639010c76fb2959600fc0e284cc41f3
manhminno <manh6264@gmail.com>
2020-11-30 22:25:30 +0700

    Add remove friend tracking functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
-end
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
