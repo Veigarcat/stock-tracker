71ea05a7da67166761eeeb710364cbd69c7cc461
huy-xu <nhathuyhedspi@gmail.com>
2020-12-01 12:53:36 +0700

    Fix search stock and add API key

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def my_portfolio
     @user = current_user
     @tracked_stocks = current_user.stocks
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,10 +5,13 @@ class Stock < ApplicationRecord
     validates :name, :ticker, presence: true
 
     def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key], endpoint: 'https://sandbox.iexapis.com/v1')
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+            secret_token: Rails.application.credentials.iex_client[:sandbox_secret_key],
+            endpoint: 'https://sandbox.iexapis.com/v1')
         begin
             new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-            #rescue => exception
+            rescue => exception
             return nil
         end
     end
