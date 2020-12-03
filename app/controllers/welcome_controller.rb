044f708cf86b7c26f9e0be2fb879b7552113ef1f
huy-xu <nhathuyhedspi@gmail.com>
2020-12-03 10:59:05 +0700

    Create Landing Page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
+    if current_user
+      redirect_to my_portfolio_path
+    end
   end
 end
